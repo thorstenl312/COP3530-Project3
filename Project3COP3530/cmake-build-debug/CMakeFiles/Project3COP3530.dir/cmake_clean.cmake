f44f30bd2a67a41cc39a09f6febe82e33f463904
Thorsten Lannynd <thorstenlannynd@gmail.com>
2021-12-06 21:24:20 -0500

    code

---
+++ Project3COP3530/cmake-build-debug/CMakeFiles/Project3COP3530.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project3COP3530.dir/main.cpp.obj"
+  "Project3COP3530.exe"
+  "Project3COP3530.exe.manifest"
+  "Project3COP3530.pdb"
+  "libProject3COP3530.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project3COP3530.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Project3COP3530/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+// CSV Reading from stackoverflow user sastanin (https://stackoverflow.com/questions/1120140/how-can-i-read-and-parse-csv-files-in-c)
+enum class CSVState {
+    UnquotedField,
+    QuotedField,
+    QuotedQuote
+};
+vector<string> readCSVRow(const string &row) {
+    CSVState state = CSVState::UnquotedField;
+    vector<string> fields {""};
+    size_t i = 0; // index of the current field
+    for (char c : row) {
+        switch (state) {
+            case CSVState::UnquotedField:
+                switch (c) {
+                    case ',': // end of field
+                        fields.push_back(""); i++;
+                        break;
+                    case '"': state = CSVState::QuotedField;
+                        break;
+                    default:  fields[i].push_back(c);
+                        break; }
+                break;
+            case CSVState::QuotedField:
+                switch (c) {
+                    case '"': state = CSVState::QuotedQuote;
+                        break;
+                    default:  fields[i].push_back(c);
+                        break; }
+                break;
+            case CSVState::QuotedQuote:
+                switch (c) {
+                    case ',': // , after closing quote
+                        fields.push_back(""); i++;
+                        state = CSVState::UnquotedField;
+                        break;
+                    case '"': // "" -> "
+                        fields[i].push_back('"');
+                        state = CSVState::QuotedField;
+                        break;
+                    default:  // end of quote
+                        state = CSVState::UnquotedField;
+                        break; }
+                break;
+        }
+    }
+    return fields;
+}
+vector<vector<string>> readCSV(ifstream &in) {
+    vector<vector<string>> table;
+    string row;
+    while (!in.eof()) {
+        getline(in, row);
+        if (in.bad() || in.fail()) {
+            break;
+        }
+        auto fields = readCSVRow(row);
+        table.push_back(fields);
+    }
+    return table;
+}
+
+int main()
+{
+    string filePath = "CLEAN_charity_data.csv";
+    vector<vector<string>> charityData;
+
+    ifstream inFile(filePath);
+    if (inFile.is_open()) {
+        charityData = readCSV(inFile);
+    }
+
+    for (int i = 0; i < charityData.size(); i++)
+    {
+        for (int j = 0; j < charityData[i].size(); j++)
+        {
+            cout << i << " : " << charityData[i][j] << endl;
+        }
+    }
+}
